f8ddc7f30df98a1ffab34e2309c157512e469ee4
Mark Scholtz <markscholtz@gmail.com>
2012-05-30 09:46:09 +0200

    Do not source git completion script
    This is no longer necessary with the latest versions of git and zsh

--- git/completion.sh
+++ git/completion.sh
@@ -1,9 +1,11 @@
-# Uses git's autocompletion for inner commands. Assumes an install of git's
-# bash `git-completion` script at $completion below (this is where Homebrew
-# tosses it, at least).
-completion=/usr/local/etc/bash_completion.d/git-completion.bash
+# Git completion works by default with the latest version of git and zsh.
 
-if test -f $completion
-then
-  source $completion
-fi
+# # Uses git's autocompletion for inner commands. Assumes an install of git's
+# # bash `git-completion` script at $completion below (this is where Homebrew
+# # tosses it, at least).
+# completion=`brew --prefix`/etc/bash_completion.d/git-completion.bash
+
+# if test -f $completion
+# then
+#   source $completion
+# fi
