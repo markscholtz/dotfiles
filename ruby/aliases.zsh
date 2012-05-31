e0388b727ac4573a04f0236f8ef957f75b9bf367
Mark Scholtz <markscholtz@gmail.com>
2012-05-31 10:40:30 +0200

    Remove unused ruby aliases and add "be" alias

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,7 +1,5 @@
-alias r='rvm use 1.8.7'
-
 alias sc='script/console'
+alias sdb='script/dbconsole'
 alias sg='script/generate'
-alias sd='script/destroy'
 
-alias migrate='rake db:migrate db:test:clone'
+alias be='bundle exec'
