8d9c985ca0b6d2c1678acd0acec8d22d06e3e35d
Mark Scholtz <mark@example.com>
2012-10-24 17:02:27 -0700

    A move comprehensive rbenv setup

--- rbenv/rbenv.zsh
+++ rbenv/rbenv.zsh
@@ -1,6 +1,15 @@
-export RBENV_ROOT="${HOME}/.rbenv"
-
-if [ -d "${RBENV_ROOT}" ]; then
-  export PATH="${RBENV_ROOT}/bin:${PATH}"
-  eval "$(rbenv init -)"
+if [[ -e "${HOME}/.rbenv/bin/rbenv" ]]; then
+  RBENV_ROOT=$HOME/.rbenv
+elif [[ -e "/usr/local/rbenv/bin/rbenv" ]]; then
+  RBENV_ROOT=/usr/local/rbenv
 fi
+
+export RBENV_ROOT
+export PATH="${RBENV_ROOT}/bin:${PATH}"
+
+if [[ -n "${RBENV_ROOT}" ]]; then
+  eval "$($RBENV_ROOT/bin/rbenv init -)"
+fi
+
+# If you want sources to be kept after builds, also add (useful on OSX for gdb):
+export RBENV_BUILD_ROOT=${RBENV_ROOT}/sources
