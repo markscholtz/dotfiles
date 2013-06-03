05658fd58b8dba2bfe13ebe3c1acf83d021240ed
Mark Scholtz <markscholtz@gmail.com>
2013-06-03 15:56:28 -0700

    Increase history size

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -12,8 +12,8 @@ fpath=($ZSH/zsh/functions $fpath)
 autoload -U $ZSH/zsh/functions/*(:t)
 
 HISTFILE=~/.zsh_history
-HISTSIZE=10000
-SAVEHIST=10000
+HISTSIZE=100000
+SAVEHIST=100000
 
 setopt NO_BG_NICE # don't nice background tasks
 setopt NO_HUP
