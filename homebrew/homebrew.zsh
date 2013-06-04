629f477a7aea863fa879fb43320dd9a4bba895c0
Mark Scholtz <markscholtz@gmail.com>
2013-06-04 13:12:16 -0700

    Only add homebrew to path on OS X

--- homebrew/homebrew.zsh
+++ homebrew/homebrew.zsh
@@ -1,2 +1,4 @@
-HOMEBREW_ROOT = brew --prefix
-export PATH="${HOMEBREW_ROOT}/bin:${PATH}"
+if [[ `uname` == 'Darwin' ]]; then
+  HOMEBREW_ROOT=brew --prefix
+  export PATH="${HOMEBREW_ROOT}/bin:${PATH}"
+fi
