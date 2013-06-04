cd4f980d5fe35daf1caba00ade512f501a503de9
Mark Scholtz <markscholtz@gmail.com>
2013-06-04 12:14:54 -0700

    Move bash setup into zsh specific files

---
+++ zsh/environment_varialbes.zsh
@@ -0,0 +1,18 @@
+# Add colours to ls output (OS X)
+export CLICOLOR=1
+
+# your project folder so that we can `c [tab]` to
+export PROJECTS=~/code
+
+# Make sure mysql executable is available
+# export PATH=$PATH:$HOME/bin:/usr/local/mysql/bin
+# # Make sure git executable is available
+# export PATH=$PATH:$HOME/bin:/usr/local/git/bin
+# # Make sure exuberant Ctags executable is available
+# export PATH="/usr/local/bin:$PATH"
+# # Make sure the shell scripts in my Dropbox folder are available
+# export PATH="$HOME/Dropbox/shellscripts:$PATH"
+# # Make sure that the correct readline is used when installing new packages
+# export LDFLAGS=-L/usr/local/Cellar/readline/6.2.1/lib
+# export CPPFLAGS=-I/usr/local/Cellar/readline/6.2.1/include
+
---
+++ zsh/shell_specifics.zsh
@@ -0,0 +1,21 @@
+# *****************************************************************************
+# Test shell type and perform any customisation based on this below
+# Idea taken from: http://shreevatsa.wordpress.com/2008/03/30/zshbash-startup-files-loading-order-bashrc-zshrc-etc/
+# *****************************************************************************
+if [[ -n $PS1 ]]; then
+  : # These are executed only for interactive shells
+  echo "SHELL: interactive"
+else
+  : # Only for NON-interactive shells
+  echo "SHELL: NON-interactive"
+fi
+
+# Doens't work in zsh!
+# if shopt -q login_shell ; then
+#   : # These are executed only when it is a login shell
+#   echo "SHELL: login"
+# else
+#   : # Only when it is NOT a login shell
+#   echo "SHELL: nonlogin"
+# fi
+
