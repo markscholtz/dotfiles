2d4d8e9f81b1e82050c5957b82121349b7a54fbe
Mark Scholtz <markscholtz@gmail.com>
2013-06-05 11:07:29 -0700

    Modify PATH

--- zsh/environment_varialbes.zsh
+++ zsh/environment_varialbes.zsh
@@ -4,15 +4,18 @@ export CLICOLOR=1
 # your project folder so that we can `c [tab]` to
 export PROJECTS=~/code
 
-# Make sure mysql executable is available
+# # Make sure mysql executable is available
 # export PATH=$PATH:$HOME/bin:/usr/local/mysql/bin
+
 # # Make sure git executable is available
 # export PATH=$PATH:$HOME/bin:/usr/local/git/bin
+
 # # Make sure exuberant Ctags executable is available
 # export PATH="/usr/local/bin:$PATH"
-# # Make sure the shell scripts in my Dropbox folder are available
-# export PATH="$HOME/Dropbox/shellscripts:$PATH"
-# # Make sure that the correct readline is used when installing new packages
-# export LDFLAGS=-L/usr/local/Cellar/readline/6.2.1/lib
-# export CPPFLAGS=-I/usr/local/Cellar/readline/6.2.1/include
+
+if [[ `uname` == 'Darwin' ]]; then
+  # Make sure that the correct readline is used when installing new packages (OS X)
+  export LDFLAGS=-L/usr/local/Cellar/readline/6.2.1/lib
+  export CPPFLAGS=-I/usr/local/Cellar/readline/6.2.1/include
+fi
 
