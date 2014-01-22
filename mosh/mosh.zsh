2587454177e6e650aaa5a8057fcfcd00526035b6
Mark Scholtz <markscholtz@gmail.com>
2014-01-21 22:57:56 -0800

    Some housekeeping
    * Moved zsh specific setup into .zshenv
    * Renamed mosh file so that it is auto sourced
    * Load NVM

--- mosh/mosh.zsh
+++ mosh/mosh.zsh
@@ -1,6 +1,7 @@
 # Ensure mosh is available when "moshing" in.
 PATH="/usr/local/bin:$PATH"
 
+# Ensure UTF-8 is set up correctly for mosh.
 # export LC_CTYPE=en_US.UTF-8
 # export LC_ALL=en_US.UTF-8
 
--- system/env.zsh
+++ system/env.zsh
@@ -58,47 +58,9 @@ LS_COLORS+="or=${green}:"                             # Symbolic Link pointing t
 LS_COLORS+="pi=${orange};${black_background}:"        # Fifo file
 LS_COLORS+="so=${green};${black_background}:"         # Socket file
 
-fpath=($ZSH/zsh/functions $fpath)
-
-autoload -U $ZSH/zsh/functions/*(:t)
-
 HISTFILE=~/.zsh_history
-HISTSIZE=100000
-SAVEHIST=100000
+HISTSIZE=10000
+SAVEHIST=7000 # Different from HISTSIZE due to ZSH HIST_EXPIRE_DUPS_FIRST option.
 
-setopt NO_BG_NICE # don't nice background tasks
-setopt NO_HUP
-setopt NO_LIST_BEEP
-setopt LOCAL_OPTIONS # allow functions to have local options
-setopt LOCAL_TRAPS # allow functions to have local traps
-setopt HIST_VERIFY
-setopt SHARE_HISTORY # share history between sessions ???
-setopt EXTENDED_HISTORY # add timestamps to history
-setopt PROMPT_SUBST
-setopt CORRECT
-setopt COMPLETE_IN_WORD
-setopt IGNORE_EOF
-
-setopt APPEND_HISTORY # adds history
-setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
-setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
-setopt HIST_REDUCE_BLANKS
-
-# don't expand aliases _before_ completion has finished
-#   like: git comm-[tab]
-setopt complete_aliases
-
-zle -N newtab
-
-bindkey -v # use vi key bindings
-
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
-bindkey '^[[5D' beginning-of-line
-bindkey '^[[5C' end-of-line
-bindkey '^[[3~' delete-char
-bindkey '^[^N' newtab
-bindkey '^?' backward-delete-char
-bindkey '^R' history-incremental-search-backward
-bindkey '^F' history-incremental-search-forward
+[ -s $HOME/.nvm/nvm.sh ] && . $HOME/.nvm/nvm.sh # This loads NVM
 
