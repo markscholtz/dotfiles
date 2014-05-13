288f07fb61c9af925aa180262c5db4492113369e
Mark Scholtz <markscholtz@gmail.com>
2014-05-12 22:58:50 -0700

    Fix google cloud sdk config

--- google-cloud-sdk/google-cloud-sdk.zsh
+++ google-cloud-sdk/google-cloud-sdk.zsh
@@ -1,8 +1,8 @@
 # The next line updates PATH for the Google Cloud SDK.
-source /usr/local/google/home/scholtzie/code/google-cloud-sdk/path.zsh.inc
+source $HOME/code/google-cloud-sdk/path.zsh.inc
 
 # The next line enables zsh completion for gcloud.
-source /usr/local/google/home/scholtzie/code/google-cloud-sdk/completion.zsh.inc
+source $HOME/code/google-cloud-sdk/completion.zsh.inc
 
-alias goapp=~/code/google-cloud-sdk/platform/google_appengine/goapp
+alias goapp=$HOME/code/google-cloud-sdk/platform/google_appengine/goapp
 
