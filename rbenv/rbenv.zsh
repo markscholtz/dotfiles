c5155e726baad6a7501e5d84374cff8a0f8f03a4
Mark Scholtz <markscholtz@gmail.com>
2016-01-31 21:20:41 -0800

    Make sure RBENV_ROOT is correctly set

--- rbenv/rbenv.zsh
+++ rbenv/rbenv.zsh
@@ -1,5 +1,7 @@
 if [[ -e "${HOME}/.rbenv/bin/rbenv" ]]; then
   RBENV_ROOT=$HOME/.rbenv
+elif [[ -e "/usr/local/bin/rbenv" ]]; then
+  RBENV_ROOT=/usr/local
 elif [[ -e "/usr/local/rbenv/bin/rbenv" ]]; then
   RBENV_ROOT=/usr/local/rbenv
 fi
