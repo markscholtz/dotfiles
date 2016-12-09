6fd519d3263fd8310fba823998097c5aa7d32593
Mark Scholtz <markscholtz@gmail.com>
2016-12-09 15:58:01 -0800

    Add tmuxinator zsh completion

--- tmuxinator/tmuxinator.zsh
+++ tmuxinator/tmuxinator.zsh
@@ -1,1 +1,32 @@
-[[ -s $HOME/.tmuxinator/scripts/tmuxinator ]] && source $HOME/.tmuxinator/scripts/tmuxinator
+# Completion script copied from tmuxinator repo. See
+# https://github.com/tmuxinator/tmuxinator#completion for more info.
+
+_tmuxinator() {
+  local commands projects
+  commands=(${(f)"$(tmuxinator commands zsh)"})
+  projects=(${(f)"$(tmuxinator completions start)"})
+
+  if (( CURRENT == 2 )); then
+    _describe -t commands "tmuxinator subcommands" commands
+    _describe -t projects "tmuxinator projects" projects
+  elif (( CURRENT == 3)); then
+    case $words[2] in
+      copy|debug|delete|open|start)
+        _arguments '*:projects:($projects)'
+      ;;
+    esac
+  fi
+
+  return
+}
+
+compdef _tmuxinator tmuxinator mux
+alias mux="tmuxinator"
+
+# Local Variables:
+# mode: Shell-Script
+# sh-indentation: 2
+# indent-tabs-mode: nil
+# sh-basic-offset: 2
+# End:
+# vim: ft=zsh sw=2 ts=2 et
