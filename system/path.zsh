c27f41e7804d675c22a338fba6121acc2bf4c5e1
Mark Scholtz <markscholtz@gmail.com>
2017-04-03 13:27:35 -0700

    Prepend $HOME/bin

--- system/path.zsh
+++ system/path.zsh
@@ -1,20 +1,16 @@
-echo "paths ..."
-path-show
-
 # Prepend
 PATH=".:$PATH"
 PATH="bin:$PATH"
 PATH="/usr/local/bin:$PATH"
 PATH="/usr/local/sbin:$PATH"
 PATH="$HOME/.sfs:$PATH"
+PATH="$HOME/bin:$PATH"
 PATH="$ZSH/bin:$PATH"
 PATH="/usr/local/heroku/bin:$PATH"
 PATH="$HOME/local/bin:$PATH"
 
 # Append
-PATH="$PATH:$HOME/bin"
 PATH="$PATH:/usr/local/mysql/bin"
 
 # Man paths
 MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-
