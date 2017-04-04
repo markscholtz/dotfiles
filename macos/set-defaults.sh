7f98f41636c5602ffc78f23a7e79f1e91388607c
Mark Scholtz <markscholtz@gmail.com>
2017-04-04 13:24:58 -0700

    Revert disabling of press-and-hold

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -5,9 +5,6 @@
 #
 # Run ./set-defaults.sh and you'll be good to go.
 
-# Disable press-and-hold for keys in favor of key repeat.
-defaults write -g ApplePressAndHoldEnabled -bool false
-
 # Use AirDrop over every interface.
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 
