e13a2654d7eb0c902b7d5ee986a3a9010f0304a8
Mark Scholtz <markscholtz@gmail.com>
2017-04-04 12:41:54 -0700

    Add a stripped down version of Holman's macos/set-defaults.sh

---
+++ macos/set-defaults.sh
@@ -0,0 +1,18 @@
+# Sets reasonable macOS defaults.
+#
+# The original idea (and a couple settings) were grabbed from:
+#   https://github.com/mathiasbynens/dotfiles/blob/master/.macos
+#
+# Run ./set-defaults.sh and you'll be good to go.
+
+# Disable press-and-hold for keys in favor of key repeat.
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+# Use AirDrop over every interface.
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
+
+# Always open everything in Finder's column view.
+defaults write com.apple.Finder FXPreferredViewStyle clmv
+
+# Show the ~/Library folder.
+chflags nohidden ~/Library
