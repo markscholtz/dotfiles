3cb9bb2bc26a49aba8fda67da186b42b1e72ac3a
Mark Scholtz <markscholtz@hey.com>
2021-01-06 11:37:13 -0800

    Remove work specific configs

--- mosh/mosh.zsh
+++
@@ -1,7 +0,0 @@
-# Ensure mosh is available when "moshing" in.
-PATH="/usr/local/bin:$PATH"
-
-# Ensure UTF-8 is set up correctly for mosh.
-# export LC_CTYPE=en_US.UTF-8
-# export LC_ALL=en_US.UTF-8
-
