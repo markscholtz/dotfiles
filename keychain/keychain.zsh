25bb970230e1c1de3ba4514e7a47938011371460
Mark Scholtz <markscholtz@hey.com>
2021-01-06 11:37:13 -0800

    Remove work specific configs

--- keychain/keychain.zsh
+++
@@ -1,9 +0,0 @@
-# -----------------------------------------------------------------------------
-# Use Keychain on linux to save ssh passphrase
-# -----------------------------------------------------------------------------
-if [[ `uname` == 'Linux' ]]; then
-  # TODO: Test that keychain exists: http://stackoverflow.com/questions/592620/check-if-a-program-exists-from-a-bash-script
-  keychain id_rsa #2>/dev/null
-  source ~/.keychain/`uname -n`-sh
-fi
-
