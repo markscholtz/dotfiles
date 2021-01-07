77ac1c6a45facfb09ca9f5fdbede5a281f836393
Mark Scholtz <scholtzie@google.com>
2021-01-06 18:02:47 -0800

    Delete workstation specific fzf config
    On my workstation fzf is installed [using git][1]. The config being
    removed here is only needed on my workstation and so I'm migrating it to
    my work local dotfiles repo.
    
    [1]: https://github.com/junegunn/fzf#using-git

--- fzf/fzf.zsh
+++ fzf/fzf.zsh
@@ -1,7 +1,3 @@
-# Installed fzf manually using git which adds the ~/.fzf.zsh file.
-# Here the file is sourced if it exists.
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-
 # "If you're running fzf in a large git repository, git ls-tree can boost up the speed of the traversal."
 # Taken from:
 # https://github.com/junegunn/fzf/blob/6b27554cdb4742b3776dc1a7a4b7590c640e7779/README.md#git-ls-tree-for-fast-traversal
