14efdcc92fc7a2a66113d239a7fa6f415f1f0830
Mark Scholtz <scholtzie@google.com>
2021-01-08 11:32:13 -0800

    Enable fzf key bindings and fuzzy completion
    Regardless of how fzf is installed, the [install instructions][1] always call for running the install script which generates `~/.fzf.zsh`. This change checks for the existense of this file and sources it if available to provide key bindings and fuzzy completion.
    
    [1]: https://github.com/junegunn/fzf#installation

--- fzf/fzf.zsh
+++ fzf/fzf.zsh
@@ -1,3 +1,7 @@
+# Load fzf key bindings and fuzzy completion if available (installation details
+# defined in local `install.conf.yaml`).
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
 # "If you're running fzf in a large git repository, git ls-tree can boost up the speed of the traversal."
 # Taken from:
 # https://github.com/junegunn/fzf/blob/6b27554cdb4742b3776dc1a7a4b7590c640e7779/README.md#git-ls-tree-for-fast-traversal
