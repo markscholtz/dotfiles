53ea4d8b86b865ade331e718a50b57f7b1a667a5
Mark Scholtz <scholtzie@google.com>
2022-05-11 05:06:25 +0000

    Remove unnecessary fzf optimization for large git repos
    This isn't a scenario I've run into and I need a totally different
    solution for dealing with my work repo so just removing this for now. I
    can always investigate and add back an optimized FZF_DEFAULT_COMMAND in
    the future if necessary.

--- fzf/fzf.zsh
+++ fzf/fzf.zsh
@@ -1,11 +1,3 @@
 # Load fzf key bindings and fuzzy completion if available (installation details
 # defined in local `install.conf.yaml`).
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-
-# "If you're running fzf in a large git repository, git ls-tree can boost up the speed of the traversal."
-# Taken from:
-# https://github.com/junegunn/fzf/blob/6b27554cdb4742b3776dc1a7a4b7590c640e7779/README.md#git-ls-tree-for-fast-traversal
-export FZF_DEFAULT_COMMAND='
-  (git ls-tree -r --name-only HEAD ||
-   find . -path "*/\.*" -prune -o -type f -print -o -type l -print |
-      sed s/^..//) 2> /dev/null'
